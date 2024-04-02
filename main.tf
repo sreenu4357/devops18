428dccc5c44c144f28a36a1b3a28755c0e45b20f
sreenu4357 <141114849+sreenu4357@users.noreply.github.com>
2024-04-02 15:06:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "web_server_as" {
-    image_id           = "ami-0830c9faf0efc29ff"
+    image_id           = "ami-0c101f26f147fa7fd"
     instance_type = "t2.micro"
-    key_name = "manish"
+    key_name = "sreenu"
 }
    
 
@@ -9,7 +9,7 @@ resource "aws_launch_configuration" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-09f13356f04fffb8b", "subnet-0f8c7425356725a9f"]
+     subnets = ["subnet-08ccf95e25657ee53", "subnet-013272d01c5dd9f78"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
@@ -28,7 +28,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-west-1a", "us-west-1b"] 
+    availability_zones    = ["us-east-1d", "us-east-1f"] 
     
   }
 
