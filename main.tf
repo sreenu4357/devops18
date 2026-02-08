b736747f5ec142c0f8534f44d1b134183f30831d
sreenu4357 <141114849+sreenu4357@users.noreply.github.com>
2026-02-08 11:36:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-  resourcresource "aws_launch_template" "web_server_lt" {
+  resource "aws_launch_template" "web_server_lt" {
   name_prefix   = "web-server-lt-"
   image_id      = "ami-0532be01f26a3de55"
   instance_type = "t3.small"
