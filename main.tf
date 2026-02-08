dc44a14ce7abbe7a5382c7b11022e52a3b1dc130
sreenu4357 <141114849+sreenu4357@users.noreply.github.com>
2026-02-08 11:29:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,22 @@
-resource "aws_launch_configuration" "web_server_as" {
-    image_id           = "ami-0532be01f26a3de55"
-    instance_type = "t3.small"
-    key_name = "docker1"
+  resourcresource "aws_launch_template" "web_server_lt" {
+  name_prefix   = "web-server-lt-"
+  image_id      = "ami-0532be01f26a3de55"
+  instance_type = "t3.small"
+  key_name      = "docker1"
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.web_server.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "web-server"
+    }
+  }
 }
-   
-
-
-  resource "aws_elb" "web_server_lb"{
+resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
      subnets = ["subnet-0deebf104e2c5f5b6", "subnet-042db347b997baee5"]
@@ -22,7 +32,7 @@ resource "aws_launch_configuration" "web_server_as" {
   }
 resource "aws_autoscaling_group" "web_server_asg" {
     name                 = "web-server-asg"
-    launch_configuration = aws_launch_configuration.web_server_as.name
+    launch_configuration = aws_launch_configuration.web_server_lt.name
     min_size             = 1
     max_size             = 3
     desired_capacity     = 2
