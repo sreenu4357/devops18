3593b38ce4e46465be58e5671b772a23658d54a4
sreenu4357 <141114849+sreenu4357@users.noreply.github.com>
2026-02-08 11:34:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,29 +16,34 @@
     }
   }
 }
-resource "aws_elb" "web_server_lb"{
-     name = "web-server-lb"
-     security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-0deebf104e2c5f5b6", "subnet-042db347b997baee5"]
-     listener {
-      instance_port     = 8000
-      instance_protocol = "http"
-      lb_port           = 80
-      lb_protocol       = "http"
-    }
-    tags = {
-      Name = "terraform-elb"
-    }
-  }
 resource "aws_autoscaling_group" "web_server_asg" {
-    name                 = "web-server-asg"
-    launch_configuration = aws_launch_configuration.web_server_lt.name
-    min_size             = 1
-    max_size             = 3
-    desired_capacity     = 2
-    health_check_type    = "EC2"
-    load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1a", "us-east-1f"] 
-    
+  name                 = "web-server-asg"
+  min_size             = 1
+  max_size             = 3
+  desired_capacity     = 2
+  health_check_type    = "EC2"
+  load_balancers       = [aws_elb.web_server_lb.name]
+  availability_zones   = ["us-east-1a", "us-east-1f"]
+
+  launch_template {
+    id      = aws_launch_template.web_server_lt.id
+    version = "$Latest"
   }
+}
+resource "aws_elb" "web_server_lb" {
+  name            = "web-server-lb"
+  security_groups = [aws_security_group.web_server.id]
+  subnets         = ["subnet-0deebf104e2c5f5b6", "subnet-042db347b997baee5"]
+
+  listener {
+    instance_port     = 8000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  tags = {
+    Name = "terraform-elb"
+  }
+}
 
